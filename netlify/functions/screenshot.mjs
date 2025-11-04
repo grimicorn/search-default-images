c40d5abcbd9675fa498aa7efc4e14398f31e0f1b
Danny Holloran <DTHolloran@Gmail.com>
2025-11-04 14:11:06 -0600

    wip

--- netlify/functions/screenshot.mjs
+++ netlify/functions/screenshot.mjs
@@ -2,15 +2,24 @@ import puppeteer from "puppeteer";
 
 export async function renderScreenshotWithPuppeteer(url) {
   const browser = await puppeteer.launch();
-
   const page = await browser.newPage();
   await page.goto(url);
   const screenshot = await page.screenshot({
     encoding: "binary",
     type: "jpeg",
   });
-
   await browser.close();
-
   return screenshot;
 }
+
+const handler = async (req, context) => {
+  const { url } = await req.json();
+
+  const screenshot = await renderScreenshotWithPuppeteer(url);
+  console.log(screenshot);
+  return new Response(screenshot, {
+    headers: { "content-type": "image/jpeg" },
+  });
+};
+
+export default handler;
