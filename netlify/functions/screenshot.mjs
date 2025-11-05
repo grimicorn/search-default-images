81dbef2bc1e886ed6d3393eaf02515de91208f60
Danny Holloran <DTHolloran@Gmail.com>
2025-11-05 10:03:17 -0600

    wip

--- netlify/functions/screenshot.mjs
+++ netlify/functions/screenshot.mjs
@@ -1,7 +1,12 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import { getStore } from "@netlify/blobs";
 
 export async function renderScreenshotWithPuppeteer(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_PATH,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+  });
   const page = await browser.newPage();
   await page.goto(url);
   const screenshot = await page.screenshot({
@@ -12,14 +17,38 @@ export async function renderScreenshotWithPuppeteer(url) {
   return screenshot;
 }
 
-const handler = async (req, context) => {
-  const { url } = await req.json();
-
-  const screenshot = await renderScreenshotWithPuppeteer(url);
-  console.log(screenshot);
-  return new Response(screenshot, {
-    headers: { "content-type": "image/jpeg" },
+const getDefaultImagesStore = () => {
+  return getStore("default-images", {
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_ACCESS_TOKEN,
   });
 };
 
+const handler = async (_req, context) => {
+  // @todo Only allow *.crossroads.net + Netlify preview for the URL param  and requester.
+  // @todo Read Me: How to install Chrome locally + error messaging to direct to the read me
+  const store = getDefaultImagesStore();
+  const url = new URL(context.url).searchParams.get("url");
+  let image = "";
+
+  try {
+    image = await store.get(url, { type: "stream" });
+
+    if (!image) {
+      image = await renderScreenshotWithPuppeteer(url);
+      await store.set(url, image);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    return new Response(image, {
+      headers: { "content-type": "image/jpeg" },
+    });
+  }
+};
+
 export default handler;
+
+export const config = {
+  path: "/api/screenshot",
+};
